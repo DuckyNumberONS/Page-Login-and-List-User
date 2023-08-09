073d2f0626a5d23d5a54cd6dcb095d0c0142b2ae
Hoang Minh Duc <duchm.bhsoft@gmail.com>
2023-08-09 14:54:56 +0700

    feat(setup): update env

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,8 +1,9 @@
+import { API } from '@/lib/constants';
 import axios from 'axios';
 
 export const getUser = async () => {
     try {
-        const response = await axios.get('https://64d20691f8d60b1743614c1a.mockapi.io/user');
+        const response = await axios.get(API);
         const users = response.data;
         return users;
     } catch (error) {
